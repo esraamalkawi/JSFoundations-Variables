9fe5e1622d699ee3d562e0f4f9654c387a796225
esraamalkawi <esraamalkawi462@gmail.com>
2021-05-17 05:56:08 -0700

    variables

---
+++ story.js
@@ -0,0 +1,4 @@
+let name = "esraa";
+let age = 24;
+let hobby = "Sing";
+console.log (`mrs. ${name}in 33 years, you will be ${age= age+33}years old. by then you will have decades of${hobby}practice!`)
